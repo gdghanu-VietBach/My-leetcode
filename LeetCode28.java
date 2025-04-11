06b3e31354b6c8de2c460fcb631b045d4ac8593f
gdghanu-VietBach <gdghanuvietbach@gmail.com>
2025-04-12 01:00:29 +0700

    quã mệt mõi
    Một bài trên Leetcode do tôi hoàn toàn tự nghĩ ra. Mặc dù nó ko tối ưu về thời gian cho lắm nma thực sự vẫn rất vui vì đã cố gắng

---
+++ LeetCode28.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+public class LeetCode28 {
+
+	// idea: ta sẽ sánh từng kí tự một để tìm ra đoạn giống nhau
+	public int strStr(String haystack, String needle) {
+
+		int checkNeeedle = 0;
+		int checkHaystack = 0;
+
+		while (checkHaystack < haystack.length()) {
+			if (haystack.charAt(checkHaystack) == needle.charAt(checkNeeedle)) {
+
+				checkHaystack++;
+				checkNeeedle++;
+
+				if (checkNeeedle == needle.length()) {
+					return checkHaystack - checkNeeedle; // nếu đã tìm ra được đoạn giống nhau, trả lại vị trí đầu tiên
+															// giống nhau
+				}
+			} else {
+
+				checkHaystack = checkHaystack - checkNeeedle + 1; // có 2TH xảy ra: + TH1: Nếu đã giống nhau 1 đoạn
+																	// trước đó thì setup về vị trí tiếp theo của vtri
+																	// đầu tiên
+				checkNeeedle = 0; // giống nhau
+				// + TH2: Nếu chưa thì xét đến vtri tiếp theo (tránh
+				// vòng
+				// lặp vô hạn vì vtri xét ko thay đổi)
+
+			}
+		}
+
+		return -1;
+	}
+}
