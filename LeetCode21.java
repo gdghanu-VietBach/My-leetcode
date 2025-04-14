ae105f2a887768067f8b03fda6bd30336d626253
gdghanu-VietBach <gdghanuvietbach@gmail.com>
2025-04-14 15:12:22 +0700

    1 bài học về đệ quy và ctdl ListNode =))))))))

--- LeetCode21.java
+++ LeetCode21.java
@@ -1,12 +1,14 @@
 package LeetCode;
 
 public class LeetCode21 {
+
+	// chuỗi sẽ được nối từ cuối lên (sau mỗi bước đệ quy sẽ nối thêm 1 nút ở đầu vào chuỗi)
 	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
 		if (list1 != null && list2 != null) {
 			if (list1.val < list2.val) {
 				list1.next = mergeTwoLists(list1.next, list2);
-				return list1; // gán gtri của list1 cho nút tiếp theo (khi đệ quy "trở về")
+				return list1; // gán giá trị list 1 cho nút đầu tiếp theo (khi unwinding đệ quy "trở về")
 			} else {
 				list2.next = mergeTwoLists(list1, list2.next);
 				return list2;
